2cec361eb1724719b95a13b186e1bb51ec53b63b
Kelsey Green <kelseygreen@Kelseys-MacBook-Pro.local>
2018-11-09 13:27:29 -0800

    fixed accordion width

--- js	/script.js
+++ js	/script.js
@@ -1,41 +1,24 @@
 
-// Open the Modal
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
+// Slide in contact info
 
-// Close the Modal
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
- 
-}
+$(document).ready(function() {
+      
+          $(window).scroll( function(){
+      
+              $('.hideme').each( function(i){
+            
+                     var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                    var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+                 /* If the object is completely visible in the window, fade in */
+                  if( bottom_of_window > bottom_of_object ){
+                
+                      $(this).animate({'opacity':'1'},5500);
+                    
+                  }
+            
+              }); 
+    
+          });
+    
+      });
